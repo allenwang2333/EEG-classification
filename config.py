506e18120323531df8dc2ec51311c9e6bcb2c8a1
Allen Wang <allenwang23333@gmail.com>
2024-03-13 22:49:39 -0700

    add data augmention

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ parser.add_argument('--lr', type=float, default=1e-3, help='Learning rate')
 parser.add_argument('--momentum', type=float, default=0.9, help='Momentum')
 parser.add_argument('--seed', type=int, default=3407, help='Random seed')
 parser.add_argument('--device', type=str, default='mps', help='Device to train on')
-parser.add_argument('--data_aug', type=bool, default=False, help='Whether to use data augmentation')
+parser.add_argument('--augment', default=None, nargs='+', choices=['gaussian', 'channel_drop'], help='Whether to use data augmentation')
 parser.add_argument('--model', type=str, default=None, help='Model to use')
 
 FLAGS = parser.parse_args()
